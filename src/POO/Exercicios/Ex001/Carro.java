07c419fae8bf8784df6d6c3e5f718e5ad819866b
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-08-13 17:21:42 -0300

    Refactor Carro class with encapsulation and constructor
    Changed Carro fields to private and added getter/setter methods for encapsulation. Introduced a constructor to initialize Carro objects and updated TesteCarro to use the new constructor. Methods in Carro are now public for proper access.

--- src/POO/Exercicios/Ex001/Carro.java
+++ src/POO/Exercicios/Ex001/Carro.java
@@ -1,15 +1,15 @@
 package POO.Exercicios.Ex001;
 
 public class Carro {
-    String marca;
-    String modelo;
-    int ano;
-    String cor;
-    int hp;
-    double torque;
-    boolean ligado;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String cor;
+    private int hp;
+    private double torque;
+    private boolean ligado;
 
-    void status() {
+    public void status() {
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Marca: " + this.marca);
         System.out.println("Ano: " + this.ano);
@@ -18,21 +18,90 @@ public class Carro {
         System.out.println("Torque: " + this.torque);
     }
 
-    void ligar () {
+    public Carro(String m, String mrk, String c, int a, int h, double t, boolean l) {
+        this.modelo = m;
+        this.marca = mrk;
+        this.cor = c;
+        this.ano = a;
+        this.hp = h;
+        this.torque = t;
+        this.ligado = l;
+
+
+    }
+
+    public void ligar () {
         this.ligado = true;
     }
 
-    void desligar() {
+    public void desligar() {
         this.ligado = false;
     }
 
-    void acelerar() {
+    public void acelerar() {
         if (this.ligado == true) {
             System.out.println("VRUUUUUUUUUUUUUUUUUUUUM!");
         } else {
             System.out.println("Erro! De partida no motor!");
         }
     }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String m) {
+        this.modelo = m;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String mrk) {
+        this.marca = mrk;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String c){
+        this.cor = c;
+    }
+
+    public int getAno() {
+        return this.ano;
+    }
+
+    public void setAno(int a) {
+        this.ano = a;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public void setHp(int h) {
+        this.hp = h;
+    }
+
+    public double getTorque() {
+        return this.torque;
+    }
+
+    public void setTorque(int t) {
+        this.torque = t;
+    }
+
+    public boolean getLigado() {
+        return this.ligado;
+    }
+
+    public void setLigado(boolean l) {
+        this.ligado = l;
+    }
+
 
 
 }
--- src/POO/Exercicios/Ex001/TesteCarro.java
+++ src/POO/Exercicios/Ex001/TesteCarro.java
@@ -2,16 +2,15 @@ package POO.Exercicios.Ex001;
 
 public class TesteCarro {
     public static void main(String[] args) {
-        Carro car1 = new Carro ();
-        car1.marca = "McLaren";
-        car1.modelo = "750s";
-        car1.ano = 2023;
-        car1.cor = "Laranja";
-        car1.hp = 750;
-        car1.torque = 81.6;
-        car1.ligado = true;
+        Carro car1 = new Carro ("750s", "McLaren", "Laranja", 2023, 750, 81.6, true);
         car1.status();
         car1.acelerar();
+        System.out.println("");
+        Carro car2 = new Carro ("CLA 45S", " Mercedes-AMG", "Mountain Grey Magno", 2023, 421, 51, true);
+        car2.status();
+        car2.acelerar();
+
+
 
     }
 }
