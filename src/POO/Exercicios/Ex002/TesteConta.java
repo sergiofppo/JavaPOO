fb16e88b698f06412afed2f8de77cecc0d862176
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-08-15 18:49:17 -0300

    Refactor TesteConta with detailed account operations
    Expanded the main method in TesteConta to include step-by-step demonstrations of account creation, deposit, withdrawal, monthly fee payment, and account closure. Added descriptive output for each operation to improve clarity and facilitate testing.

--- src/POO/Exercicios/Ex002/TesteConta.java
+++ src/POO/Exercicios/Ex002/TesteConta.java
@@ -2,11 +2,34 @@ package POO.Exercicios.Ex002;
 
 public class TesteConta {
     public static void main(String[] args) {
-        Conta c1 = new Conta(444, "Sergio Filippo");
-        c1.abrirConta("cc");
-        c1.depositar(1800);
-        c1.sacar(500);
-        c1.pagarMensalidade();
-        c1.consultarSaldo();
+        System.out.println("---------- Criando Conta Corrente ----------");
+        Conta conta1 = new Conta(1001, "Sérgio");
+        conta1.abrirConta("cc");
+        conta1.consultarSaldo();
+
+        System.out.println("---------- Depósito ----------");
+        conta1.depositar(200);
+        conta1.consultarSaldo();
+
+        System.out.println("---------- Saque ----------");
+        conta1.sacar(100);
+        conta1.consultarSaldo();
+
+        System.out.println("---------- Pagando Mensalidade ----------");
+        conta1.pagarMensalidade();
+        conta1.consultarSaldo();
+
+        System.out.println("---------- Tentando Fechar Conta com Saldo > 0 ----------");
+        conta1.fecharConta();
+
+        System.out.println("---------- Zerando Saldo ----------");
+        conta1.sacar((float) conta1.getSaldoConta());
+        conta1.consultarSaldo();
+
+        System.out.println("---------- Fechando Conta corretamente ----------");
+        conta1.fecharConta();
+
+
+        System.out.println("\n-----------------------------\n");
     }
 }
