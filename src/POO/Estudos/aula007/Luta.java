570219d77988f35540d4fb72a355e4a3bfb7ef13
Sérgio Filippo <m4n0x.br0@gmail.com>
2025-08-26 18:43:37 -0300

    Refactor Luta to use Lutador objects and update tests
    Changed Luta class to use Lutador objects instead of String names for fighters, updated fight logic to determine winner randomly, and improved fight approval checks. TesteLutador was updated to use new fighter names and demonstrate the new Luta workflow.

--- src/POO/Estudos/aula007/Luta.java
+++ src/POO/Estudos/aula007/Luta.java
@@ -1,24 +1,62 @@
 package POO.Estudos.aula007;
 
+import java.util.Random;
+
 public class Luta {
-    private String desafiado;
-    private String desafiate;
+    private Lutador desafiado;
+    private Lutador desafiante;
     private int rounds;
     private boolean aprovada;
 
-    public void marcarLuta() {
-
+    public void marcarLuta(Lutador l1, Lutador l2) {
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2) {
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+            System.out.println("Luta Marcada!");
+        }  else {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+            System.out.println("Luta Desaprovada!");
+        }
     }
 
     public void lutar () {
-        
+        if (aprovada) {
+            this.desafiado.apresentar();
+            this.desafiante.apresentar();
+
+            Random rand = new Random();
+            int vencedor = rand.nextInt(3);
+
+            switch (vencedor) {
+                case 0:
+                    System.out.println("Empatou!");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1:
+                    System.out.println(desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2:
+                    System.out.println(desafiante.getNome());
+                    this.desafiado.perderLuta();
+                   this.desafiante.ganharLuta();
+                    break;
+            }
+        } else {
+            System.out.println("Luta Invalida!");
+        }
     }
 
-    public String getDesafiado() { return this.desafiado; }
-    public void setDesafiado(String dedo) { this.desafiado = dedo; }
+    public Lutador getDesafiado() { return this.desafiado; }
+    public void setDesafiado(Lutador dedo) { this.desafiado = dedo; }
 
-    public String getDesafiate() { return this.desafiate; }
-    public void setDesafiate(String dante) { this.desafiate = dante; }
+    public Lutador getDesafiante() { return this.desafiante; }
+    public void setDesafiante(Lutador dante) { this.desafiante = dante; }
 
     public int getRounds() { return this.rounds; }
     public void setRounds(int r) { this.rounds = r; }
--- src/POO/Estudos/aula007/TesteLutador.java
+++ src/POO/Estudos/aula007/TesteLutador.java
@@ -3,24 +3,27 @@ package POO.Estudos.aula007;
 public class TesteLutador {
     public static void main(String[] args) {
         Lutador l[] = new Lutador[6];
-        l[0] = new Lutador("Pretty Boy", "Franca", 31, 1.75f, 68.9f, 11, 2, 1);
+        l[0] = new Lutador("Prefedi", "Franca", 31, 1.75f, 68.9f, 11, 2, 1);
 
-        System.out.println("----------");
-        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+        l[1] = new Lutador("Cenoura Pequeno", "Brasil", 18, 1.80f, 57.8f, 14, 2, 3);
 
-        System.out.println("----------");
-        l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
+        l[2] = new Lutador("Sombra Branca", "EUA", 35, 1.65f, 80.9f, 12, 2, 1);
 
-        System.out.println("----------");
-        l[3] = new Lutador("Dead Code", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
 
-        System.out.println("----------");
-        l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
+        l[3] = new Lutador("Bree Picante", "Austrália", 28, 1.93f, 81.6f, 13, 0, 2);
 
-        System.out.println("----------");
-        l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
+        l[4] = new Lutador("Sergio O Midia", "Brasil", 19, 1.81f, 82.3f, 22, 0, 0);
+
+
+        l[5] = new Lutador("Pegs O leve", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
+
+        Luta UFC01 = new Luta();
+
+        UFC01.marcarLuta(l[0], l[1]);
+        UFC01.lutar();
 
         l[1].Status();
+
 
     }
 }
